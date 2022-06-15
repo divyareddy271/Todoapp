7f156bd83bb08bbff462be09036c6e2e4ea10380
divya reddy <divyareddy271999@gmail.com>
2022-06-15 13:54:12 +0530

    added add show notification and mark as complete code

--- todo.js
+++ todo.js
@@ -5,12 +5,44 @@ const tasksCounter = document.getElementById('tasks-counter');
 
 console.log('Working');
 
-function renderList () {}
 
-function markTaskAsComplete (taskId) {}
+function renderList () {
+   // tasks.map((task) =>) 
+}
+
+function markTaskAsComplete (taskId) {
+    const index = tasks.indexOf(taskId);
+    tasks[index].done = true;
+}
 
 function deleteTask (taskId) {}
 
-function addTask (task) {}
+function addTask (task) {
+    tasks.push(task);
+    console.log(tasks.length);
+    renderList();
+}
 
-function showNotification(text) {}
+function showNotification(text) {
+    alert(text);
+}
+function Handleeventlistner(e) {
+  
+    if(e.key === "Enter"){
+        const text = e.target.value;
+        console.log(text);
+        if(!text){
+            showNotification("Task text cannot be empty")
+            return;
+        }
+        const task =  {
+            text,
+            id : Date.now().toString(),
+            done : false
+        }
+        addTask(task);
+        e.target.value="";
+    }
+    
+}
+addTaskInput.addEventListener('keyup',Handleeventlistner)
